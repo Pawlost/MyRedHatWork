2105f638e42a14ff3f466342d255d151c41708ba
Pavel Balusek <mr.p.balusek@gmail.com>
2018-05-31 20:09:29 +0200

    BETA Version of roulette

--- roulette/src/main/java/BetObject.java
+++ roulette/src/main/java/BetObject.java
@@ -7,20 +7,29 @@ public class BetObject {
 	private int moneyValue;
 	private boolean hasBet = false;
 	
-	public BetObject(int orderNumber) {
-	   this.orderNumber = orderNumber;
+	public BetObject(int orderNumber, int reward) {
+		this.moneyValue = reward;
+		this.orderNumber = orderNumber;
 	}
 	
 	public void bet(int moneyValue) {
 		hasBet = true;
-		this.moneyValue = moneyValue;
+		this.moneyValue = this.moneyValue + moneyValue;
 	}
 	
-	public int getOrder() {
-		return orderNumber;
+	public void setReward(int newReward) {
+		this.moneyValue = newReward;
+	}
+	
+	public String getOrder() {
+		return String.valueOf(orderNumber);
 	}
 	
 	public int getReward() {
 		return moneyValue;
+	}
+	
+	public boolean isBet() {
+		return hasBet;
 	}
 }
--- roulette/src/main/java/GameCreator.java
+++ roulette/src/main/java/GameCreator.java
@@ -1,53 +1,120 @@
 package main.java;
 
-
 import java.util.ArrayList;
 import java.util.Random;
 
-import javax.enterprise.inject.Produces;
-import javax.faces.bean.RequestScoped;
-import javax.inject.Inject;
+import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Named;
 
 @Named
-@RequestScoped
+@ApplicationScoped
 public class GameCreator {
-
-    @Produces
-    @Named
 	private ArrayList<BetObject> bets;
-	
-	private static int money = 1000;
+	private final int liveCount = 5;
+	private int money = 1000;
+	private int draws = 3;
 	private static final int MAX_REWARD = 1000;
 	private static final int MIN_REWARD = 500;
-	private int reward;
+	private String message;
+	private int reward;  
 	private Random rand;
+	private boolean hasBet =false;
+
 	
 	public GameCreator() {
 		rand = new Random();
 		bets = new ArrayList<BetObject>();
-		bets.add(new BetObject(1));
-		bets.add(new BetObject(2));
 		reward = rand.nextInt(MAX_REWARD) + MIN_REWARD;
+		bets.add(new BetObject(bets.size() + 1, reward));
+		bets.add(new BetObject(bets.size() + 1, reward));
+		bets.add(new BetObject(bets.size() + 1, reward));
 	}
 	
-	private void game() {
-		if(money > 0) {
-			
+	public void game() {
+		if(hasBet) {
+			message = "";
+			if(money <= 0) {
+				if (bets.size() >= liveCount){
+				message = "Prohrál jsi";
+			}else {
+				message = "Jsi na nule, musíš získat peníze";
+			}
+		}else {
+			if(bets.size() <= 0){
+				message = "Vyhrál jsi";
+			}else{
+				for(int i=0; i<draws; i++) {
+					int winnigNumber = rand.nextInt(10*bets.size());
+					try {
+						BetObject currentBet = bets.get(winnigNumber);
+						if(currentBet.isBet()) {
+							money += currentBet.getReward();
+							message = message + " Vyhrál jsi " + currentBet.getReward() +" na číslo " + winnigNumber;
+						}else {
+							message = message +  "Nic jsi nevyhrál";
+						}
+					}catch(Exception e){
+						message = message +  "Nic jsi nevyhrál";
+					}
+				}
+			}
 		}
 		reward = rand.nextInt(MAX_REWARD) + MIN_REWARD;
+		hasBet=false;
+		}else {
+			message = "Nevsadil jsi";
+		}
 	}
 	
-	public void pressButton(String id) {
-		
+	public void betButton(String maneyValueString, String idString){
+		try {
+			int moneyValue = Integer.parseInt(maneyValueString);
+			int id = Integer.parseInt(idString);
+			if (moneyValue < money && money > 0) {
+				hasBet = true;
+				message = "Vsadil jsi " + maneyValueString + " na cislo " + idString;
+				bets.get(id-1).bet(moneyValue);
+				money -= moneyValue;
+			} else {
+				message = "Nemáš tolik peněz";
+			}
+		} catch(Exception e){
+			message = "Napiš číslo";
+		}
 	}
-
+	
+	public void addBetButton() {
+		bets.add(new BetObject(bets.size() + 1, reward));
+		money += 500;
+	}
+	
+	public void destroyBetButton () {
+		if(money > 10000) {
+			bets.remove(bets.size()-1);
+			money -= 10000;
+		}
+	}
 	
 	public String getMoney() {
 		return "Zrovna máš " + String.valueOf(money) + " peněz";
 	}
 	
-	public ArrayList<BetObject> getObject(){
+	public ArrayList<BetObject> getBets(){
 		return bets;
+	}
+	
+	public String getMessage() {
+		return message;
+	}
+	
+	public String getDraws() {
+		return "Budou se vytahovat "+ String.valueOf(draws)+ " čísla" ;
+	}
+	public String getHasBet() {
+		if(hasBet) {
+			return "Máš vazeno";
+		}else {
+			return "Nemáš vsazeno";
+		}
 	}
 }
